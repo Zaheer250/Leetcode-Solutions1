41558f1a1b5da82e9ba956d89d9d35b94385aca5
Mohammad Zaheer <114864416+Zaheer250@users.noreply.github.com>
2024-09-14 14:16:38 +0530

    Time: 2 ms (97.64%) | Memory: 41.8 MB (99.72%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.java
+++ 3567-convert-date-to-binary/convert-date-to-binary.java
@@ -1,33 +1,14 @@
 class Solution {
     public String convertDateToBinary(String date) {
-        String a="";
-        String b="";
-        String c="";
-        int i=0;
-        while(date.charAt(i) != '-'){
-            a+=date.charAt(i);
-            i+=1;
-        }
-        i+=1;
-        while(date.charAt(i) != '-'){
-            b+=date.charAt(i);
-            i+=1;
-        }
-        i+=1;
-        while(i!=date.length()){
-            c+=date.charAt(i);
-            i+=1;
-        }
-
-        int a1 = Integer.parseInt(a);
-        int b1 = Integer.parseInt(b);
-        int c1 = Integer.parseInt(c);
-
-        String a2=Integer.toBinaryString(a1);
-        String b2=Integer.toBinaryString(b1);
-        String c2=Integer.toBinaryString(c1);
-
-        String ans = a2 + "-" + b2 + "-" + c2;
-        return ans;
+        String y = date.substring(0,4);
+        String m = date.substring(5,7);
+        String d = date.substring(8,10);
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.toBinaryString(Integer.parseInt(y)));
+        sb.append("-");
+        sb.append(Integer.toBinaryString(Integer.parseInt(m)));
+        sb.append("-");
+        sb.append(Integer.toBinaryString(Integer.parseInt(d)));
+        return sb.toString();
     }
 }
