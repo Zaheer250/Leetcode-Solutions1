5f910789da8941e2e95ec817170398188878d5e1
Mohammad Zaheer <114864416+Zaheer250@users.noreply.github.com>
2024-10-05 14:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (97.30%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,33 +1,24 @@
 class Solution {
-    public String mergeAlternately(String word1, String word2) {
-        String str="";
-        int a=0,b=0;
-        int n1=word1.length();
-        int n2=word2.length();
-        while(a<n1 && b<n2){
-            str+=word1.charAt(a);
-            str+=word2.charAt(b);
-            a+=1;
-            b+=1;
+     public static String mergeAlternately(String word1, String word2) {
+        int wordPointer1 = 0;
+        int wordPointer2 = 0;
+        StringBuilder merged = new StringBuilder();
+
+        int minLength = Math.min(word1.length(), word2.length());
+
+        for (int i = 0; i < minLength; i++){
+            merged.append(word1.charAt(wordPointer1));
+            merged.append(word2.charAt(wordPointer2));
+            wordPointer1++;
+            wordPointer2++;
         }
 
-        if(n1 == n2){
-            return str;
-        }
-        else if(n1 > n2){
-            while(a<n1){
-                str+=word1.charAt(a);
-                a+=1;
-            }
-        }
-        else{
-            while(b<n2){
-                str+=word2.charAt(b);
-                b+=1;
-            }
+        if (wordPointer1 >= word1.length()){
+            merged.append(word2.substring(wordPointer2, word2.length()));
+        } else if (wordPointer2 >= word2.length()){
+            merged.append(word1.substring(wordPointer1, word1.length()));
         }
 
-        return str;
-
+        return merged.toString();
     }
 }
