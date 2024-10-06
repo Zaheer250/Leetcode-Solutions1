547c158b56c0ad7f8197bf74a9185f850e66a825
Mohammad Zaheer <114864416+Zaheer250@users.noreply.github.com>
2024-10-06 22:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (67.44%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,59 +1,36 @@
 class Solution {
-    public class Edge{
-        int src;
-        int dest;
-
-        public Edge(int src, int dest){
-            this.src=src;
-            this.dest=dest;
-        }
-    }
     public boolean isBipartite(int[][] graph) {
-        ArrayList<Edge> ad_graph[] = new ArrayList[graph.length];
-        for(int i = 0; i < graph.length; i++) {
-           ad_graph[i] = new ArrayList<>();
-        }
-        createGraph(ad_graph, graph);
-        return isBipartiteUtil(ad_graph);
-    }
+        if (graph.length == 0) return true;
+        int[] colors = new int[graph.length];
 
-
-    //MAIN LOGIC 
-    public boolean isBipartiteUtil(ArrayList<Edge> graph[]){
-        int col[]=new int[graph.length];
-        for(int i=0;i<col.length;i++){
-            col[i]=-1;  //No colour
-        }
-        Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<graph.length;i++){
-            if(col[i]==-1){
-                q.add(i);
-                col[i]=0; // 0=red, 1=blue, -1=no colour
-                while(!q.isEmpty()){
-                    int curr=q.remove();
-                    for(int j=0;j<graph[curr].size();j++){
-                        Edge e=graph[curr].get(j);
-                        if(col[e.dest] == -1){ // No colour = add colour to node opposite to its parent
-                            int nextCol = col[curr] == 0 ? 1 : 0;
-                            col[e.dest]=nextCol;
-                            q.add(e.dest);
-                        }
-                        else if(col[e.dest] == col[curr]){  //Adjacent node have same colour = NOT A BIPARTITE
-                            return false;
-                        }
-                    }
-                }
+        for (int i = 0; i < graph.length; i++) {
+            if (colors[i] == 0) {
+                if(!dfs(graph, i, colors, 1)){
+                    return false;
+                } 
             }
         }
+
         return true;
     }
 
-    public void createGraph(ArrayList<Edge> ad_graph[], int[][] graph){
-        for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                Edge edge = new Edge(i, graph[i][j]);
-                ad_graph[i].add(edge);
+
+    public boolean dfs(int[][] graph, int i, int[] colors, int color) {
+        // neighbour should not be same as node
+        if(colors[i] > 0) {
+            return colors[i] == color;
+        }
+        
+        colors[i] = color;
+
+        int neightbourColor = colors[i] == 1 ? 2 : 1;
+
+        for (int j = 0; j < graph[i].length; j++) {
+            if(!dfs(graph, graph[i][j], colors, neightbourColor)) {
+                return false;
             }
         }
+
+        return true;
     }
 }
