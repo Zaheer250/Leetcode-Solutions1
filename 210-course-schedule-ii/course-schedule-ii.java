0b543e03c4a11edc41af687f58e892dc7a6706b8
Mohammad Zaheer <114864416+Zaheer250@users.noreply.github.com>
2024-10-07 22:50:45 +0530

    Time: 6 ms (57.34%) | Memory: 46 MB (39.64%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    public class Edge{
+        int src;
+        int dest;
+        public Edge(int src, int dest){
+            this.src=src;
+            this.dest=dest;
+        }
+    }
+
+    public void createGraph(ArrayList<Edge>[] graph, int[][] prerequisites){
+        for(int i=0;i<prerequisites.length;i++){
+            int a=prerequisites[i][0];
+            int b=prerequisites[i][1];
+
+            Edge e=new Edge(b,a);
+            graph[b].add(e);
+        }
+    }
+
+    public void calcIndeg(ArrayList<Edge>[] graph, int[] indegree){
+        for(int i=0;i<graph.length;i++){
+            for(int j=0;j<graph[i].size();j++){
+                Edge e=graph[i].get(j);
+                indegree[e.dest]++;
+            }
+        }
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<Edge>[] graph=new ArrayList[numCourses];
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<>();
+        }
+        createGraph(graph, prerequisites);
+        List<Integer> temp=new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
+        int[] indegree=new int[graph.length];
+        calcIndeg(graph, indegree);
+        
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+        
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            temp.add(curr);
+            
+            for(int i=0;i<graph[curr].size();i++){
+                Edge e=graph[curr].get(i);
+                indegree[e.dest]--;
+                if(indegree[e.dest] == 0){
+                    q.add(e.dest);
+                }
+            }
+        }
+        if(temp.size() < numCourses){
+            return new int[]{};
+        }
+        int[] ans=new int[temp.size()];
+        int idx=0;
+        for(int val:temp){
+            ans[idx++]=val;
+        }
+
+        return ans;
+    }
+}
